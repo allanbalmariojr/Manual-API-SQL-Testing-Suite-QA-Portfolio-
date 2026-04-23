ce5ed22280e27054b2f9f4850126d736be5a3f49
allanbalmariojr <allanbalmariojr@gmail.com>
2026-04-23 16:38:08 -0700

    Update main.py

--- Python_Automation/main.py
+++ Python_Automation/main.py
@@ -1,1 +1,57 @@
-
+import data
+import helpers
+ 
+ 
+class TestUrbanRoutes:
+    # Task 4
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes Server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+ 
+    # Task 3
+    def test_set_route(self):
+        # Add in S8
+        print("function created for set route function")
+        pass
+ 
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for select plan function")
+        pass
+ 
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for fill phone number function")
+        pass
+ 
+    def test_fill_card(self):
+        # Add in S8
+        print("function created for fill card function")
+        pass
+ 
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment for driver function")
+        pass
+ 
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("function created for order blanket and handkerchiefs function")
+        pass
+ 
+    def test_car_search_model(self):
+        # Add in S8
+        print("function created for car search model function")
+        pass
+ 
+    # Task 5
+    def test_order_2_ice_creams(self):
+        # Add in S8
+        for number in range(0, 2):
+            # Add in S8
+            pass
+        print("function created for order 2 ice creams function")
+        pass
